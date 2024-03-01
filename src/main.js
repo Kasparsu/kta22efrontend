cb759619bdc31aade0638ac5ff0ef37761e51346
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-01 15:32:46 +0200

    Add vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import './style.css';
 import App from './App.vue';
 
@@ -9,8 +10,30 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
 const vuetify = createVuetify({
-  components,
-  directives,
+    components,
+    directives,
 });
 
-createApp(App).use(vuetify).mount('#app');
+
+import Home from './pages/Home.vue';
+import Register from './pages/Register.vue';
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+    { path: '/', component: Home },
+    { path: '/register', component: Register },
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+    // 4. Provide the history implementation to use. We
+    // are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+});
+
+
+createApp(App).use(vuetify).use(router).mount('#app');
